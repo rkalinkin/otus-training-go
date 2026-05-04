ab7479c242b209f9629e3bae045a16115619380c
Roman Kalinkin <r.kalinkin@vk.team>
2026-05-04 22:09:48 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,37 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	if len(words) == 0 {
+		return nil
+	}
+
+	wordCounts := make(map[string]int, len(words))
+	for _, word := range words {
+		wordCounts[word]++
+	}
+
+	uniqueWords := make([]string, 0, len(wordCounts))
+	for word := range wordCounts {
+		uniqueWords = append(uniqueWords, word)
+	}
+
+	sort.Slice(uniqueWords, func(i, j int) bool {
+		if wordCounts[uniqueWords[i]] != wordCounts[uniqueWords[j]] {
+			return wordCounts[uniqueWords[i]] > wordCounts[uniqueWords[j]]
+		}
+		return uniqueWords[i] < uniqueWords[j]
+	})
+
+	resultLimit := 10
+	if len(uniqueWords) > resultLimit {
+		uniqueWords = uniqueWords[:resultLimit]
+	}
+
+	return uniqueWords
 }
